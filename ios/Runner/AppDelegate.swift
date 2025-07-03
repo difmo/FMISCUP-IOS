ca77b077dbb142d0605092c8ac34965e2dcb667d
7844Pritam <pritamsharma7844@gmail.com>
2025-07-03 15:48:24 +0530

    save

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,36 @@
-import Flutter
 import UIKit
+import Flutter
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+    let alarmChannel = FlutterMethodChannel(name: "alarm_channel", binaryMessenger: controller.binaryMessenger)
+
+    alarmChannel.setMethodCallHandler { (call: FlutterMethodCall, result: @escaping FlutterResult) in
+      switch call.method {
+      case "setAlarms":
+        print("setAlarms called on iOS")
+        result("Alarm set (stub) on iOS")
+        
+      case "requestExactAlarmPermission":
+        print("requestExactAlarmPermission called on iOS")
+        result("Not required on iOS")
+
+      case "cancelAlarms":
+        print("cancelAlarms called on iOS")
+        result("Alarms canceled (stub) on iOS")
+        
+      default:
+        result(FlutterMethodNotImplemented)
+      }
+    }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
