35168f0d2459af8b36e4cccc02672e0c0dcd7497
7844Pritam <pritamsharma7844@gmail.com>
2025-09-05 20:40:56 +0530

    save

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.techwings.fmiscup"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.0.12"
+        versionCode = 13
+        versionName = "1.0.13"
     }
 
     signingConfigs {
